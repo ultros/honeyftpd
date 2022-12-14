9deac2f74d9a09fe289dc7250366169245405f65
Jesse Shelley <jesse@cybertutorials.org>
2022-12-14 14:09:14 -0700

    Initial commit

---
+++ ftpdhoney.py
@@ -0,0 +1,29 @@
+import logging
+
+import pyftpdlib.authorizers
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+
+class DummyAuthorizerCollectCreds(DummyAuthorizer):
+
+    def validate_authentication(self, username, password, handler):
+        with open("ftpdhoney-credentials.txt", 'a') as file_obj:
+            file_obj.write(f"{handler.addr[0]}:{username}:{password}\n")
+        raise pyftpdlib.authorizers.AuthenticationFailed
+
+
+def main():
+    logging.basicConfig(level=logging.FATAL)
+    authorizer = DummyAuthorizerCollectCreds()
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    handler.banner = "vsFTPD 2.3.4"
+    address = ('', 21)
+    server = FTPServer(address, handler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
